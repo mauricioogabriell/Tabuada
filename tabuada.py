033301da5ea35ae82d9947513653fe21552eff3b
Mauricio Gabriel <131178240+mauricioogabriell@users.noreply.github.com>
2023-05-11 09:04:05 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,15 @@
+print('seja bem vindo,ao primeiro projeto de uma tabuada feito em phyton')
+print('obs: essa tabuada vai até 1 ao 10')
+n1=int(input('digite um número, para criarmos a sua tabuada = '))
+a=n1*1
+b=n1*2
+c=n1*3
+d=n1*4
+e=n1*5
+f=n1*6
+g=n1*7
+h=n1*8
+i=n1*9
+j=n1*10
+
+print( 'sua tabuada do numero {} está pronta\n{} x 1 = {}\n{} x 2 = {}\n{} x 3 = {}\n{} x 4 = {}\n{} x 5 = {}\n{} x 6 = {}\n{} x 7 = {}\n{} x 8 = {}\n{} x 9 = {}\n{} x 10 = {}  '.format(n1,n1,a,n1,b,n1,c,n1,d,n1,e,n1,f,n1,g,n1,h,n1,i,n1,j))
